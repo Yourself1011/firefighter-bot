2352706961cf2ed0109277976bba592bd90e0396
Daniel Zhang <d.zhang200788@gmail.com>
2025-03-04 13:34:27 -0500

    lcd

---
+++ motortest.asm
@@ -0,0 +1,163 @@
+;Program compiled by Great Cow BASIC (0.98.06 2019-06-12 (Darwin 64 bit))
+;Need help? See the GCBASIC forums at http://sourceforge.net/projects/gcbasic/forums,
+;check the documentation or email w_cholmondeley at users dot sourceforge dot net.
+
+;********************************************************************************
+
+;Set up the assembler options (Chip type, clock source, other bits and pieces)
+ LIST p=16F18875, r=DEC
+#include <P16F18875.inc>
+ __CONFIG _CONFIG1, _CLKOUTEN_OFF & _RSTOSC_HFINT32 & _FEXTOSC_OFF
+ __CONFIG _CONFIG2, _MCLRE_OFF
+ __CONFIG _CONFIG3, _WDTE_OFF
+ __CONFIG _CONFIG4, _LVP_OFF
+ __CONFIG _CONFIG5, _CP_OFF
+
+;********************************************************************************
+
+;Set aside memory locations for variables
+DELAYTEMP	EQU	112
+DELAYTEMP2	EQU	113
+SYSWAITTEMPMS	EQU	114
+SYSWAITTEMPMS_H	EQU	115
+SYSWAITTEMPS	EQU	116
+
+;********************************************************************************
+
+;Vectors
+	ORG	0
+	pagesel	BASPROGRAMSTART
+	goto	BASPROGRAMSTART
+	ORG	4
+	retfie
+
+;********************************************************************************
+
+;Start of program memory page 0
+	ORG	5
+BASPROGRAMSTART
+;Call initialisation routines
+	call	INITSYS
+
+;Start of the main program
+	bcf	TRISD,3
+	bcf	TRISD,2
+	bcf	TRISD,0
+	bcf	TRISD,1
+	bsf	TRISB,5
+	bcf	LATD,3
+	bcf	LATD,2
+	bcf	LATD,0
+	bcf	LATD,1
+	call	FW
+SysDoLoop_S1
+	btfss	PORTB,5
+	goto	ENDIF1
+	call	BW
+	movlw	1
+	movwf	SysWaitTempS
+	call	Delay_S
+	call	FW
+ENDIF1
+	goto	SysDoLoop_S1
+SysDoLoop_E1
+BASPROGRAMEND
+	sleep
+	goto	BASPROGRAMEND
+
+;********************************************************************************
+
+BW
+	bcf	LATD,3
+	bsf	LATD,2
+	bcf	LATD,0
+	bsf	LATD,1
+	return
+
+;********************************************************************************
+
+Delay_MS
+	incf	SysWaitTempMS_H, F
+DMS_START
+	movlw	14
+	movwf	DELAYTEMP2
+DMS_OUTER
+	movlw	189
+	movwf	DELAYTEMP
+DMS_INNER
+	decfsz	DELAYTEMP, F
+	goto	DMS_INNER
+	decfsz	DELAYTEMP2, F
+	goto	DMS_OUTER
+	decfsz	SysWaitTempMS, F
+	goto	DMS_START
+	decfsz	SysWaitTempMS_H, F
+	goto	DMS_START
+	return
+
+;********************************************************************************
+
+Delay_S
+DS_START
+	movlw	232
+	movwf	SysWaitTempMS
+	movlw	3
+	movwf	SysWaitTempMS_H
+	call	Delay_MS
+	decfsz	SysWaitTempS, F
+	goto	DS_START
+	return
+
+;********************************************************************************
+
+FW
+	bsf	LATD,3
+	bcf	LATD,2
+	bsf	LATD,0
+	bcf	LATD,1
+	return
+
+;********************************************************************************
+
+INITSYS
+;osccon type is 100
+	movlw	96
+	banksel	OSCCON1
+	movwf	OSCCON1
+	clrf	OSCCON3
+	clrf	OSCEN
+	clrf	OSCTUNE
+;the mcu is a chip family 15
+;osccon type is 102
+	movlw	6
+	movwf	OSCFRQ
+	banksel	ADCON0
+	bcf	ADCON0,ADFRM0
+	bcf	ADCON0,ADON
+	banksel	ANSELA
+	clrf	ANSELA
+	clrf	ANSELB
+	clrf	ANSELC
+	clrf	ANSELD
+	clrf	ANSELE
+	banksel	CM2CON0
+	bcf	CM2CON0,C2ON
+	bcf	CM1CON0,C1ON
+	banksel	PORTA
+	clrf	PORTA
+	clrf	PORTB
+	clrf	PORTC
+	clrf	PORTD
+	clrf	PORTE
+	return
+
+;********************************************************************************
+
+;Start of program memory page 1
+	ORG	2048
+;Start of program memory page 2
+	ORG	4096
+;Start of program memory page 3
+	ORG	6144
+
+ END
